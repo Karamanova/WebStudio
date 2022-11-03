564c14ef45cfb4a8fc22b26c4ee183814256b100
Maryna Karamanova <karamanova91@gmail.com>
2022-11-03 23:03:17 +0200

    corrected sections

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -18,7 +18,8 @@
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
